15520fef335542d9fad5fb6b3a5c377aa238db70
Tsutomu Kurafuji <bull.bull.bull91@gmail.com>
2017-08-10 13:16:17 +0000

    20170810_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  root 'application#hello'
+  get  'static_pages/home'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-  end
-
 end
